8b9117b67af66056d06d11b2e1343033fc54c462
Serakov Vitaliy <v.serakov@favbet.com>
2015-05-28 20:44:27 +0300

    added bc02.erl

---
+++ src/L2/bs02.erl
@@ -0,0 +1,14 @@
+-module(bs02).
+-export([words/1]).
+
+words(B) ->
+    words(B, <<>>, []).
+
+words(<<" ", Rest/binary>>, Acc1, Acc2) ->
+    words(Rest, <<>>, [Acc1 | Acc2]);
+
+words(<<B:1/binary, Rest/binary>>, Acc1, Acc2) ->
+     words(Rest, <<Acc1/binary, B:1/binary>>, Acc2);
+ 
+ words(<<>>, Acc1, Acc2) ->
+     lists:reverse([Acc1 | Acc2]).
