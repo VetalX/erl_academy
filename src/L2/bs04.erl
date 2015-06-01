45e8feb8fb281eb519c21d7a25b861ff4777e7c3
Serakov Vitaliy <v.serakov@favbet.com>
2015-06-01 10:49:25 +0300

    alpha version of xml parser L2 T4

--- src/L2/bs04.erl
+++ src/L2/bs04.erl
@@ -2,30 +2,40 @@
 -export([decode_xml/1]).
 
 decode_xml(BinXml) ->
-    decode_xml(BinXml, start_open_tag, <<>>, <<>>, []).
+    decode_xml(BinXml, open_tag, <<>>, []).
 
+decode_xml(<<"</", _Rest/binary>> = Bin, open_tag, Acc1, Acc2) ->
+    decode_xml(Bin, close_tag, Acc1, Acc2);
 
-decode_xml(<<"<", Rest/binary>>, start_open_tag, Acc, Acc1, Acc2) ->
-    decode_xml(Rest, open_tag, Acc, Acc1, Acc2);
+decode_xml(<<"<", Rest/binary>>, open_tag, Acc1, Acc2) ->
+    decode_xml(Rest, open_tag, Acc1, Acc2);
 
-decode_xml(<<">", Rest/binary>>, open_tag, Acc, Acc1, Acc2) ->
-    decode_xml(Rest, start_close_tag, <<>>, Acc1, [Acc | Acc2]);
+decode_xml(<<">", Rest/binary>>, open_tag, Acc1, Acc2) ->
+    decode_xml(Rest, body, <<>>, [<<"open_tag_", Acc1/binary>> | Acc2]);
 
-decode_xml(<<B:1/binary, Rest/binary>>, open_tag, Acc, Acc1, Acc2) ->
-    decode_xml(Rest, open_tag, <<Acc/binary, B:1/binary>>, Acc1, Acc2);
+decode_xml(<<B:1/binary, Rest/binary>>, open_tag, Acc1, Acc2) ->
+    decode_xml(Rest, open_tag, <<Acc1/binary, B:1/binary>>, Acc2);
 
-decode_xml(<<"</", Rest/binary>>, start_close_tag, Acc, Acc1, Acc2) ->
-    decode_xml(Rest, close_tag, Acc, Acc1, Acc2);
+decode_xml(<<"<", _Rest/binary>> = Bin, body, <<>>, Acc2) ->
+    decode_xml(Bin, open_tag, <<>>, Acc2);
 
-decode_xml(<<">", Rest/binary>>, close_tag, Acc, Acc1, Acc2) ->
-    decode_xml(Rest, start_open_tag, <<>>, Acc1, [Acc | Acc2]);
+decode_xml(<<"</", _Rest/binary>> = Bin, body, Acc1, Acc2) ->
+    decode_xml(Bin, close_tag, <<>>, [<<"body_", Acc1/binary>> | Acc2]);
 
-decode_xml(<<B:1/binary, Rest/binary>>, close_tag, Acc, Acc1, Acc2) ->
-    decode_xml(Rest, close_tag, <<Acc/binary, B:1/binary>>, Acc1, Acc2);
+decode_xml(<<"<", _Rest/binary>> = Bin, body, Acc1, Acc2) ->
+    decode_xml(Bin, open_tag, <<>>, [<<"body_", Acc1/binary>> | Acc2]);
 
-decode_xml(<<B:1/binary, Rest/binary>>, start_close_tag, Acc, Acc1, Acc2) ->
-    decode_xml(Rest, start_close_tag, Acc, <<Acc1/binary, B:1/binary>>, Acc2);
+decode_xml(<<B:1/binary, Rest/binary>>, body, Acc1, Acc2) ->
+    decode_xml(Rest, body, <<Acc1/binary, B:1/binary>>, Acc2);
 
-decode_xml(<<>>, _, Acc, Acc1, Acc2) ->
-    [Acc, Acc1, Acc2].
+decode_xml(<<"</", Rest/binary>>, close_tag, Acc1, Acc2) ->
+    decode_xml(Rest, close_tag, Acc1, Acc2);
 
+decode_xml(<<">", Rest/binary>>, close_tag, Acc1, Acc2) ->
+    decode_xml(Rest, open_tag, <<>>, [<<"close_tag_", Acc1/binary>> | Acc2]);
+
+decode_xml(<<B:1/binary, Rest/binary>>, close_tag, Acc1, Acc2) ->
+    decode_xml(Rest, close_tag, <<Acc1/binary, B:1/binary>>, Acc2);
+
+decode_xml(_, _, Acc1, Acc2) ->
+    {Acc1, lists:reverse(Acc2)}.
